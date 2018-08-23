26c0cf731e276cb96eed5368d8e0330995f2b09a
Mateus <mateusferreira058>
2018-08-23 14:23:33 -0300

    Criação do banco de dados

---
+++ Banco de Dados/quick.sql
@@ -0,0 +1,457 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 23-Ago-2018 às 16:58
+-- Versão do servidor: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quick`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `idAdministrador` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `rg` varchar(14) NOT NULL,
+  `telefone` varchar(13) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bairro`
+--
+
+CREATE TABLE `bairro` (
+  `idBairro` int(11) NOT NULL,
+  `nomeBairro` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `carrinho`
+--
+
+CREATE TABLE `carrinho` (
+  `idCarrinho` int(11) NOT NULL,
+  `quantidade` int(2) NOT NULL,
+  `valorUnitario` double NOT NULL,
+  `valorTotal` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidade`
+--
+
+CREATE TABLE `cidade` (
+  `idCidade` int(11) NOT NULL,
+  `nomeCidade` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `rg` varchar(14) NOT NULL,
+  `telefone` varchar(14) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `idEndereco` int(11) NOT NULL,
+  `nomeRua` varchar(45) NOT NULL,
+  `numeroResidencia` smallint(6) NOT NULL,
+  `cep` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estado`
+--
+
+CREATE TABLE `estado` (
+  `idEstado` int(11) NOT NULL,
+  `nomeEstado` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historicocompras`
+--
+
+CREATE TABLE `historicocompras` (
+  `idHistoricoCompras` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itemvenda`
+--
+
+CREATE TABLE `itemvenda` (
+  `idItemVenda` int(11) NOT NULL,
+  `quantidade` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marca`
+--
+
+CREATE TABLE `marca` (
+  `idMarca` int(11) NOT NULL,
+  `nomeMarca` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `opcaopagamento`
+--
+
+CREATE TABLE `opcaopagamento` (
+  `idOpcaoPagamento` int(11) NOT NULL,
+  `cartao` varchar(30) NOT NULL,
+  `dinheiro` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `operador`
+--
+
+CREATE TABLE `operador` (
+  `idOperador` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `rg` varchar(14) NOT NULL,
+  `telefone` varchar(13) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(11) NOT NULL,
+  `quantidade` int(2) NOT NULL,
+  `dataPedido` date NOT NULL,
+  `horaPedido` time NOT NULL,
+  `valorUnitario` double NOT NULL,
+  `valorTotal` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `idProduto` int(11) NOT NULL,
+  `nomeProduto` varchar(60) NOT NULL,
+  `valorUnitario` double NOT NULL,
+  `descricaoProduto` varchar(255) NOT NULL,
+  `peso` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `relatoriovenda`
+--
+
+CREATE TABLE `relatoriovenda` (
+  `idRelatorioVendas` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `salvarlista`
+--
+
+CREATE TABLE `salvarlista` (
+  `idSalvarLista` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `validarpedido`
+--
+
+CREATE TABLE `validarpedido` (
+  `idValidarPedido` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `idVenda` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`idAdministrador`);
+
+--
+-- Indexes for table `bairro`
+--
+ALTER TABLE `bairro`
+  ADD PRIMARY KEY (`idBairro`);
+
+--
+-- Indexes for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD PRIMARY KEY (`idCarrinho`);
+
+--
+-- Indexes for table `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`idCidade`);
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`idEndereco`);
+
+--
+-- Indexes for table `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indexes for table `historicocompras`
+--
+ALTER TABLE `historicocompras`
+  ADD PRIMARY KEY (`idHistoricoCompras`);
+
+--
+-- Indexes for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  ADD PRIMARY KEY (`idItemVenda`);
+
+--
+-- Indexes for table `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`idMarca`);
+
+--
+-- Indexes for table `opcaopagamento`
+--
+ALTER TABLE `opcaopagamento`
+  ADD PRIMARY KEY (`idOpcaoPagamento`);
+
+--
+-- Indexes for table `operador`
+--
+ALTER TABLE `operador`
+  ADD PRIMARY KEY (`idOperador`);
+
+--
+-- Indexes for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`);
+
+--
+-- Indexes for table `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`idProduto`);
+
+--
+-- Indexes for table `relatoriovenda`
+--
+ALTER TABLE `relatoriovenda`
+  ADD PRIMARY KEY (`idRelatorioVendas`);
+
+--
+-- Indexes for table `salvarlista`
+--
+ALTER TABLE `salvarlista`
+  ADD PRIMARY KEY (`idSalvarLista`);
+
+--
+-- Indexes for table `validarpedido`
+--
+ALTER TABLE `validarpedido`
+  ADD PRIMARY KEY (`idValidarPedido`);
+
+--
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`idVenda`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `idAdministrador` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bairro`
+--
+ALTER TABLE `bairro`
+  MODIFY `idBairro` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  MODIFY `idCarrinho` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `cidade`
+--
+ALTER TABLE `cidade`
+  MODIFY `idCidade` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `idEndereco` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `historicocompras`
+--
+ALTER TABLE `historicocompras`
+  MODIFY `idHistoricoCompras` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  MODIFY `idItemVenda` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `idMarca` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `opcaopagamento`
+--
+ALTER TABLE `opcaopagamento`
+  MODIFY `idOpcaoPagamento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `operador`
+--
+ALTER TABLE `operador`
+  MODIFY `idOperador` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `relatoriovenda`
+--
+ALTER TABLE `relatoriovenda`
+  MODIFY `idRelatorioVendas` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `salvarlista`
+--
+ALTER TABLE `salvarlista`
+  MODIFY `idSalvarLista` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `validarpedido`
+--
+ALTER TABLE `validarpedido`
+  MODIFY `idValidarPedido` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `idVenda` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
